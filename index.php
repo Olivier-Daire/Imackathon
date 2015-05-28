c6fffe249e3c75c31bff1aa632008c8832b75823
Olivier-Daire <daire.olivier@gmail.com>
2015-05-28 12:04:40 +0200

    DB connexion

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<?php
+
+	require_once __DIR__.'/DBconnexion.php';
+	$connexion = DBConnexion::connectToDB();
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title></title>
 	</head>
 	<body>
-
 	</body>
 </html>
