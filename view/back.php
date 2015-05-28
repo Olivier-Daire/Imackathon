15fbef28e2764ecc42485ffbec857fc79d3f4729
Romain François <belum76@gmail.com>
2015-05-28 17:14:43 +0200

    menu backend

--- view/back.php
+++ view/back.php
@@ -1,30 +1,12 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Back office</title>
-</head>
+<?php include(__DIR__.'/../web/head.php'); ?>
 <body>
-	<h3>Changer de mot de passe</h3>
-	<form method="POST" action="../helpers/formHandler.php">
-		<input type="text" name="formType" value="changePassword" hidden/>
-
-					<p><label for="oldPwd">Ancien mot de passe</label></p>
-			   		<p><input type="password" class="u-full-width" name="oldPwd" id="oldPwd" required /></p>
-
-
-				   	<p><label for="newPwd">Nouveau mot de passe</label></p>
-				   	<p><input type="password" class="u-full-width" name="newPwd" id="newPwd" required/></p>
-
-				   	<p><label for="newPwd2">Retapez le nouveau mot de passe</label></p>
-				   	<p><input type="password" class="u-full-width" name="newPwd2" id="newPwd2" required/></p>
-
-		   			<button type="submit">Submit</button>
-		
-	</form>
+	<nav>
+		<ul>
+			<li><a href="./changePwd.php">changer de mot de passe</a></li>
+			<li><a href="./list.php">Gérer les XML</a></li>
+		</ul>
+	</nav>
 </body>
 </html>
