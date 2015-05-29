6d706eca8b3b6ede9fd87037b6c296f759a81898
pierrechls <pierre.chls@gmail.com>
2015-05-29 10:58:22 +0200

    MAJ script

--- view/v2.0/js/script.js
+++ view/v2.0/js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 	
+	var lastScrollTop = 0;
+	var window_height = $(window).height();
+	var window_top = $(window).scrollTop();
+	
+
+	$( window ).scroll(function() {
+		var st = $(this).scrollTop();
+		console.log(st);
+		$('#wrapper-content').css('top', 0.01 * st);
+		window_top = $(window).scrollTop();
+		lastScrollTop = st;		
+	});
+	
+	
 	function getXMLByYear( filename, year ) {
 		$.ajax({
 			type: "GET",
@@ -9,11 +23,23 @@ $(document).ready(function () {
 			success: function(xml) {
 				$(xml).find('semester').each(function(){
 					var semesterNumber = $(this).attr("number");
-				
+					
 					$("#year" + year ).append("<div class='semestre' id='semestre" + semesterNumber + "'><h1>Année " + year + ", Semestre" + semesterNumber + "</h1><div class='content'></div></div>");
-				
+					
+					var cptBackground = 1;
+					var ColorBackground;
+					
 					$(this).find("ue").each(function(){
-						$("#year" + year + " #semestre" + semesterNumber + " .content").append("<div class='column'><div class='block' style='width: 200px; height: 200px;'><h3>" + $(this).attr("name") + "</h3></div></div>");
+					
+						if(cptBackground == 1) ColorBackground = "#436D77";
+						if(cptBackground == 2) ColorBackground = "#F15D49";
+						if(cptBackground == 3){
+							ColorBackground = "#76AF50";
+							cptBackground=1;	
+						}
+					
+						$("#year" + year + " #semestre" + semesterNumber + " .content").append("<div class='column'><div class='block' style='width: 200px; height: 200px; background-color: "+ColorBackground+"'><h3>" + $(this).attr("name") + "</h3></div></div>");
+						cptBackground++;
 					
 					});
 				
