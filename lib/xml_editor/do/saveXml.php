0b2fac7ec5cd4fafab19e57173b34ca02a1f5dff
Olivier-Daire <daire.olivier@gmail.com>
2015-05-29 10:49:56 +0200

    Cleanup

--- lib/xml_editor/do/saveXml.php
+++ lib/xml_editor/do/saveXml.php
@@ -8,17 +8,15 @@
 if (isset($_POST['xmlString'])){
 	$filename  = $_POST['xmlFilename'];
 	$xmlString = stripslashes($_POST['xmlString']);
-	
-	$newFile = $filename.".edit.xml";
 
 	//write new data to the file, along with the old data 
-	$handle = fopen("../../../xml/".$newFile, "w");
+	$handle = fopen("../../../xml/".$filename, "w");
 	if (fwrite($handle, $xmlString) === false) {
 		echo json_encode(array("error"=>"Couldn't write to file."));
 	} 
 	else {
 		xmlController::updateXML($filename);
-		echo json_encode(array("filename"=>$newFile));
+		echo json_encode(array("filename"=>$filename));
 	}
 	fclose($handle); 	
 }
